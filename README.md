cd506440cc22f8a2d7dc45a5dbb6f41e477c11ee
AndresParaGO <andresparragn@gmail.com>
2020-12-29 12:13:59 -0500

    added styles to in the home and add image in the readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Simple Webpack React Boilerplate
 
 ⚡ Simple boilerplate reactjs using, webpack  4, webpack dev server, babel 7.
+
+
+![Preview](https://repository-images.githubusercontent.com/325339106/2e5f7d80-49cf-11eb-9476-40096fc4a31c)
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../styles/App.css';
+import React from 'react';
+import icon from '../assets/react.png';
+
+function App() {
+  return <div className="App">
+    <h1>⚡ Simple Webpack React Boilerplate</h1> 
+    <a className="App-btn" href="https://github.com/AndresParraGO/simple-webpack-react-boilerplate" target="_blank">
+      View in Github
+    </a>
+  </div>
+}
+
+export default App;
